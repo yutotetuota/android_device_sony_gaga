c93a2e196653cf1ebff0c0af69f8b08542129035
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2018-09-14 18:24:24 +0900

    fix yuga to gaga

--- gaga.mk
+++ gaga.mk
@@ -15,7 +15,7 @@
 # Inherit the fusion-common definitions
 $(call inherit-product, device/sony/fusion3-common/fusion3.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/sony/yuga/overlay
+DEVICE_PACKAGE_OVERLAYS += device/sony/gaga/overlay
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
@@ -68,4 +68,4 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Include non-opensource parts
-$(call inherit-product, vendor/sony/yuga/yuga-vendor.mk)
+$(call inherit-product, vendor/sony/gaga/gaga-vendor.mk)
